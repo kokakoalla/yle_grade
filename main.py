6aa1670477bcb63c4cfe27c94ab6572a4f20feb8
kokakoalla <konsta.larionov.vanced@gmail.com>
2024-01-10 18:57:10 +0200

    Add file

---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+from textblob import TextBlob
+
+resource = requests.get("https://yle.fi/uutiset")
+soup = BeautifulSoup(resource.content, "html.parser")
+
+headlines = soup.find_all("h3")
+
+news_data = []
+
+for headline in headlines:
+    news_text = headline.text
+    print("News headline:", news_text)
+
+
+    analysis = TextBlob(news_text)
+    sentiment = analysis.sentiment.polarity
+
+
+    if sentiment > 0:
+        sentiment_label = "Positive"
+    elif sentiment < 0:
+        sentiment_label = "Negative"
+    else:
+        sentiment_label = "Neutral"
+
+    news_data.append({"title": news_text, "grade": sentiment_label})
+
+    print("Тональность:", sentiment_label)
+    print('\n')
+
+print("done")
+
+
+for item in news_data:
+    print(f"Title: {item['title']}")
+    print(f"Grade: {item['grade']}")
+    print('\n')
+
